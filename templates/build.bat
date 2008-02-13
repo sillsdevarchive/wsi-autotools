416a2340a9228b3230a0ba90efda161de71416f3
Martin Hosken <martin_hosken@sil.org>
2008-02-13 04:04:31 +0000

    Fix bugs in previous changes

--- templates/build.bat
+++ templates/build.bat
@@ -1,5 +1,10 @@
 @echo off
 setlocal
+set _I=%0
+set _R=ret1
+set _D=\
+goto getpath
+:ret1
 set CFGFILE=%1
 IF NOT "%CFGFILE%"=="-d" GOTO T1
 set CFGFILE=package.cfg
@@ -11,6 +16,20 @@ set DEBUG=%2
 GOTO doit
 :T2
 set CFGFILE=package.cfg
+goto doit
+
+rem      :getpath -- figure out directory from a file with path
+:getpath
+set _I=%_I:"=%
+:getpath_l
+set _T=%_I:~-1,1%
+if %_T%X==%_D%X goto getpath_d
+if X%_I%==X goto getpath_d
+set _I=%_I:~0,-1%
+goto getpath_l
+:getpath_d
+goto %_R%
+
 
 :doit
-perl %DEBUG% templates/autosub -c %CFGFILE% -c templates/system.cfg -d . -x ~ templates/templates
+perl %DEBUG% %_I%autosub -c %CFGFILE% -c %_I%system.cfg -d . -x ~ %_I%templates
